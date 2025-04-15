87f32a2a8b5af5100d48bcce44f993294ea80b48
Nkidi John Nkogatse <nkiditoday@gmail.com>
2025-04-15 20:39:06 +0200

    [WIP] Updated the types file

--- app/types.ts
+++ app/types.ts
@@ -1,11 +1,11 @@
 export interface ChatMessage {
-    id: string;
-    role: 'user' | 'assistant';
-    content: string;
+    id: string
+    role: 'user' | 'assistant'
+    content: string
 }
 
 export interface Chat {
-    id: string;
-    title: string;
-    messages: string;
+    id: string
+    title: string
+    messages: ChatMessage[]
 }
